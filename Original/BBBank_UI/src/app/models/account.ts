c215c69f93b00edcba8fc5d252f4cfd82f64a583
waqasbt <waqas-dev@outlook.com>
2022-06-21 22:26:32 +0500

    Update Original UI Code

---
+++ Original/BBBank_UI/src/app/models/account.ts
@@ -0,0 +1,19 @@
+
+export class AccountListsResponse {
+    accounts: Array<Account>;
+    resultCount: number;
+  }
+  
+  export class Account {
+    accountTitle: string;
+    user: User;
+    currentBalance: number;
+    accountStatus: number;
+    accountNumber: string;
+  }
+  export class User {
+    profilePicUrl: string;
+    email: string;
+    phoneNumber: string;
+  }
+  
